775bfe83a445bc0095a7b30916c0a95bc3456850
Matthew White <mwhite7@lesley.edu>
2015-03-10 08:13:14 -0400

    first commit

---
+++ wp-content/themes/html5blank-stable/footer.php
@@ -0,0 +1,24 @@
+			<!-- footer -->
+			<footer class="footer" role="contentinfo">
+
+				<!-- copyright -->
+				<p class="copyright">
+					&copy; <?php echo date('Y'); ?> Copyright <?php bloginfo('name'); ?>. <?php _e('Powered by', 'html5blank'); ?>
+					<a href="//wordpress.org" title="WordPress">WordPress</a> &amp; <a href="//html5blank.com" title="HTML5 Blank">HTML5 Blank</a>.
+				</p>
+				<!-- /copyright -->
+
+			</footer>
+			<!-- /footer -->
+
+		</div>
+		<!-- /wrapper -->
+
+		<?php wp_footer(); ?>
+
+				<script src="<?php bloginfo('stylesheet_directory');?>/js/jquery-2.1.3.min.js"></script>
+
+		<script src="<?php bloginfo('stylesheet_directory');?>/bootstrap/js/bootstrap.min.js"></script>
+
+	</body>
+</html>
---
+++ wp-content/themes/html5blank-stable/header.php
@@ -0,0 +1,80 @@
+<!doctype html>
+<html <?php language_attributes(); ?> class="no-js">
+	<head>
+		<meta charset="<?php bloginfo('charset'); ?>">
+		<title><?php wp_title(''); ?><?php if(wp_title('', false)) { echo ' :'; } ?> <?php bloginfo('name'); ?></title>
+		<meta name="viewport" content="width=device-width">
+
+		<link href="//www.google-analytics.com" rel="dns-prefetch">
+        <link href="<?php echo get_template_directory_uri(); ?>/img/icons/favicon.ico" rel="shortcut icon">
+        <link href="<?php echo get_template_directory_uri(); ?>/img/icons/touch.png" rel="apple-touch-icon-precomposed">
+
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta name="description" content="<?php bloginfo('description'); ?>">
+	
+		<link rel="stylesheet" href="<?php bloginfo('stylesheet_directory');?>/bootstrap/css/bootstrap.css">
+	<?php wp_head(); ?>
+		
+		<script>
+        // conditionizr.com
+        // configure environment tests
+        conditionizr.config({
+            assets: '<?php echo get_template_directory_uri(); ?>',
+            tests: {}
+        });
+        </script>
+
+	</head>
+	<body <?php body_class(); ?>>
+
+		<!-- wrapper -->
+		<div class="container">
+
+			<!-- header -->
+			<header class="row" role="banner">
+
+					<!-- logo -->
+					<div class="logo">
+						<a href="<?php echo home_url(); ?>">
+							<!-- svg logo - toddmotto.com/mastering-svg-use-for-a-retina-web-fallbacks-with-png-script -->
+							<img src="<?php echo get_template_directory_uri(); ?>/img/logo.svg" alt="Logo" class="logo-img">
+						</a>
+					</div>
+					<!-- /logo -->
+
+					<!-- nav -->
+					<nav class="navbar navbar-default" role="navigation">
+					  <div class="container-fluid">
+					    <!-- Brand and toggle get grouped for better mobile display -->
+					    <div class="navbar-header">
+					      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+					        <span class="sr-only">Toggle navigation</span>
+					        <span class="icon-bar"></span>
+					        <span class="icon-bar"></span>
+					        <span class="icon-bar"></span>
+					      </button>
+					      <a class="navbar-brand" href="<?php echo home_url(); ?>">
+					                <?php bloginfo('name'); ?>
+					            </a>
+					    </div>
+
+					        <?php
+					            wp_nav_menu( array(
+					                'menu'              => 'primary',
+					                'theme_location'    => 'primary',
+					                'depth'             => 2,
+					                'container'         => 'div',
+					                'container_class'   => 'collapse navbar-collapse',
+					       			'container_id'      => 'bs-example-navbar-collapse-1',
+					                'menu_class'        => 'nav navbar-nav',
+					                'fallback_cb'       => 'wp_bootstrap_navwalker::fallback',
+					                'walker'            => new wp_bootstrap_navwalker())
+					            );
+					        ?>
+					    </div>
+					</nav>
+					<!-- /nav -->
+
+			</header>
+			<!-- /header -->
